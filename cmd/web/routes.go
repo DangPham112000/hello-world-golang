4ed9371f1953ff211e4560ac368ccd932ace1890
DangPham112000 <dangpham112000@gmail.com>
2023-01-20 12:08:16 +0700

    Middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,10 +6,14 @@ import (
 	"github.com/DangPham112000/hello-world-golang/pkg/config"
 	"github.com/DangPham112000/hello-world-golang/pkg/handlers"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
 
 	mux.Get("/", http.HandlerFunc(handlers.RepoC.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.RepoC.About))
