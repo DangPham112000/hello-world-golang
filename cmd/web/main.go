3de8d1bf6374b3d1f5dbb5158647f3aebde4b370
DangPham112000 <dangpham112000@gmail.com>
2023-02-07 00:17:10 +0700

    Set up session package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,16 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/DangPham112000/hello-world-golang/pkg/config"
 	"github.com/DangPham112000/hello-world-golang/pkg/handlers"
 	"github.com/DangPham112000/hello-world-golang/pkg/render"
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTeamplateCache()
 	if err != nil {
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,6 +14,7 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
 
 	mux.Get("/", http.HandlerFunc(handlers.RepoC.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.RepoC.About))
